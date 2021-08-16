7dd36757c71cea05afe950c68a81392d0c321bae
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-16 21:17:31 +0900

    백준 단계별 풀기 그리디 알고리즘

--- BAEKJOON/15. 그리디 알고리즘/주유소(13305).py
+++ BAEKJOON/15. 그리디 알고리즘/주유소(13305).py
@@ -1,26 +1,5 @@
-# import sys
-# input = sys.stdin.readline
-# N = int(input())
-# distance = list(map(int,input().split()))
-# liter = list(map(int,input().split()))
-# min_liter = liter.index(min(liter[:len(liter)-1]))
-
-# result = 0
-# for i in range(N-1):
-#     if i == min_liter and i==0:
-#         result += sum(distance[min_liter:])*liter[i]
-#         break
-#     elif liter[i] <= liter[i+1]: #현재 리터값이 다음 리터값보다 작으면
-#         if i != min_liter: #현재 리터값이 최소값이 아니면
-#             result += sum(distance[:min_liter])*liter[i]
-#             # result += sum(distance[min_liter:])*liter[min_liter]
-#             break
-#         else: # 현재 리터값이 최소값이면
-#             result+= sum(distance[i:])*liter[i]
-#             break
-#     else: #현재 리터값이 다음 리터값보다
-#         result += distance[i]*liter[i]
-# print(result)
+import sys
+input = sys.stdin.readline
 N = int(input())
 roads = list(map(int, input().split()))
 cities = list(map(int, input().split()))
@@ -28,11 +7,8 @@ cities = list(map(int, input().split()))
 minVal = cities[0]
 sum = 0
 for i in range(N-1):
-    print(i)
     if minVal > cities[i]:
-        
         minVal = cities[i]
     sum += (minVal * roads[i])
-    # print(sum)
     
 print(sum)
